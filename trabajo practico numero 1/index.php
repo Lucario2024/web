0b0d1256e483a295fa18ae64c33a937fa43d548c
Lucario2024 <163000366+Lucario2024@users.noreply.github.com>
2024-08-02 20:13:28 -0300

    trabajo practico 1 de prueva

---
+++ trabajo practico numero 1/index.php
@@ -0,0 +1,18 @@
+<?php
+$titulo = 'trabajo practico numero 1';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $titulo; ?></title>
+</head>
+<body>
+<div class="welcome-message">
+    <h1>Bienvenido a <?php echo $titulo;?></h1>
+</div>
+
+</body>
+</html> 
