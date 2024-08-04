a35b838cc748e60bf1fe802b3d783850e4f339a1
Lucario2024 <163000366+Lucario2024@users.noreply.github.com>
2024-08-03 21:36:29 -0300

    trabajo practico numero 1 segundo cuatrimestre

---
+++ trabajo practico numero 1 segundo cuatrimestre/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+    <title>formulario</title>
+</head>
+
+<body>
+<form action="procesar.php" method="post" name="frm">
+
+  <input type="text" name="nombre" />
+  <input type ="text" name="apellido" />
+  <input type="submit" value="enviar" />
+</form>
+</html>
